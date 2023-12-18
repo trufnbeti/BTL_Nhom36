2a36bd8663a9dfd392e76d3a9646dbd6fd71d04b
trufnbeti <trungboy80@gmail.com>
2023-12-18 18:34:53 +0700

    +add parallax

---
+++ Assets/TrungNguyen/_Game/Scripts/Parallax.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxEffect;
+    
+
+    private float startPos, length;
+    private float temp, dist;
+    private Vector3 newPos;
+    
+    private void Start() {
+        startPos = transform.position.x;
+        length = spriteRenderer.bounds.size.x;
+    }
+
+    private void LateUpdate() {
+        temp = cam.transform.position.x * (1 - parallaxEffect);
+        dist = cam.transform.position.x * parallaxEffect;
+        newPos.Set(startPos + dist, cam.transform.position.y - 1f, transform.position.z);
+
+        transform.position = newPos;
+
+        if (temp > startPos + length) {
+            startPos += length;
+        } else if (temp < startPos - length) {
+            startPos -= length;
+        }
+    }
+}
