7273b2bd35f51944559be9f04528cc0466e87229
TuCoSieuCap <tuphamminh282002@gmail.com>
2024-01-12 21:55:11 +0700

    +ui shop

--- Assets/TrungNguyen/_Game/Scripts/UI/UIShop.cs
+++ Assets/TrungNguyen/_Game/Scripts/UI/UIShop.cs
@@ -2,39 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using Spine.Unity;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class UIShop : UICanvas {
-	[SerializeField] private SkinData skinData;
 	[SerializeField] private SkeletonGraphic characterSkin;
+	[SerializeField] private Button btnPurchase;
+	[SerializeField] private Button btnEquip;
+	[SerializeField] private Button btnEquipped;
+	[SerializeField] private Text priceTxt;
 	
+	private SkinData skinData;
 	private List<CharacterSkin> listSkin;
-	private int price, totalSkin, index;
+	private int price, totalSkin, currentIdx;
 	private string nextSkin;
-
-	public Dictionary<string, int> test = new Dictionary<string, int>();
-
+	private CharacterSkin currentSkin;
+	private int[] skinStatus = new int[6];
+	
 	protected override void OnInit() {
+		skinData = DataManager.Ins.skinData;
+		skinStatus = DataManager.Ins.playerData.skinStatus;
 		listSkin = skinData.skins;
 		totalSkin = listSkin.Count;
-		for (int i = 0; i < totalSkin; ++i) {
-			if (listSkin[i].skinName == characterSkin.initialSkinName) {
-				index = listSkin[i].index;
-			}
-		}
-		test.Add("hi", 1);
-		Debug.Log(JsonUtility.ToJson(test));
+		DisableAllBtn();
+		currentIdx = DataManager.Ins.IdSkin;
+		characterSkin.Skeleton.SetSkin(listSkin[currentIdx].skinName);
+		currentSkin = listSkin[currentIdx];
+		btnEquipped.gameObject.SetActive(true);
 	}
 
 	public void OnBtnRightClick() {
-		index = (index + 1) % totalSkin;
-		nextSkin = listSkin[index].skinName;
+		currentIdx = (currentIdx + 1) % totalSkin;
+		currentSkin = listSkin[currentIdx];
+		nextSkin = listSkin[currentIdx].skinName;
 		characterSkin.Skeleton.SetSkin(nextSkin);
+		LoadBtn(skinStatus[currentIdx]);
 	}
 	
 	public void OnBtnLeftClick() {
-		index = (index + totalSkin - 1) % totalSkin;
-		nextSkin = listSkin[index].skinName;
+		currentIdx = (currentIdx + totalSkin - 1) % totalSkin;
+		currentSkin = listSkin[currentIdx];
+		nextSkin = currentSkin.skinName;
 		characterSkin.Skeleton.SetSkin(nextSkin);
+		LoadBtn(skinStatus[currentIdx]);
+	}
+
+	public void OnBtnPurchaseClick() {
+		DataManager.Ins.Coin -= listSkin[currentIdx].price;
+		DisableAllBtn();
+		btnEquip.gameObject.SetActive(true);
+		skinStatus[currentIdx] = 1;
+		DataManager.Ins.SkinStatus = skinStatus;
+	}
+
+	public void OnBtnEquipClick() {
+		DisableAllBtn();
+		btnEquipped.gameObject.SetActive(true);
+		skinStatus[DataManager.Ins.IdSkin] = 1;
+		skinStatus[currentIdx] = 2;
+		DataManager.Ins.IdSkin = currentIdx;
+	}
+
+	public void OnBtnBackClick() {
+		this.PostEvent(EventID.MainMenu);
+		CloseDirectly();
+	}
+
+	private void DisableAllBtn() {
+		btnPurchase.gameObject.SetActive(false);
+		btnEquip.gameObject.SetActive(false);
+		btnEquipped.gameObject.SetActive(false);
+	}
+	
+
+	private void LoadBtn(int index) {
+		switch (index) {
+			case 0:
+				DisableAllBtn();
+				btnPurchase.gameObject.SetActive(true);
+				priceTxt.text = currentSkin.price.ToString();
+				btnPurchase.interactable = (DataManager.Ins.Coin >= listSkin[currentIdx].price) ? true : false;
+				break;
+			case 1:
+				DisableAllBtn();
+				btnEquip.gameObject.SetActive(true);
+				break;
+			case 2:
+				DisableAllBtn();
+				btnEquipped.gameObject.SetActive(true);
+				break;
+		}
 	}
 }
 
